722c4b6d64739dbe8beb8fe25dce3e0580189664
lms <1454335861@qq.com>
2024-04-19 10:03:27 +0800

    fix: og-image

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 export const title = 'Discover and Explore: Your Comprehensive Search Hub for US Seniors'
 export const appName = 'woogle'
-export const ogImage = 'https://img.woogle.info/og-image.png'
+export const ogImage = 'https://assets.b1.games/article/share_image/woogle.png'
 export const keyWords =
     'Senior-friendly search, Aging population information, US seniors exploration, Tailored search for elderly, Knowledge for older adults, Elderly-friendly search platform, American seniors navigation, Age-appropriate search results, Elderly online discovery, Senior citizens web exploration, Senior-friendly search engine, Elderly information portal, Aging population resources, US seniors online exploration, Tailored search for older adults, Age-appropriate web content, Elder-friendly search experience, American seniors knowledge hub, Silver surfers online guide, Senior citizen information hub, Elderly lifestyle searches, US seniors digital discovery, Retirement-age web exploration, Age-sensitive search results, Elderly-focused online navigation, Aging gracefully online tips, Golden years information platform, Senior citizens digital journey, Elderly-specific online resources, Silver generation search insights'
 export const appDescription =
